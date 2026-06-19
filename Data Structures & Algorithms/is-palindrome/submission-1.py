d006cbff3190daa5e4aa0bfb6b4749dc9c7a5a6a
Ayush Jha <31466652+AJ-Dev-Labs@users.noreply.github.com>
2026-06-19 15:02:00 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        strs = ""
+        lowS = s.lower()
+
+        for c in lowS:
+            dig = ord(c)
+            if((dig >= 97 and dig <= 122) or (dig >=48 and dig <= 57)):
+                strs = strs+c
+        print(strs)
+        print(ord("0"))
+
+        left = 0
+        right = len(strs) - 1
+
+        while(left < right):
+            if(strs[left] != strs[right]):
+                return False
+            else:
+                left +=1
+                right -=1
+        return True
+
+        
